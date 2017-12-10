d888cba597e9a078a1ef3ca592e37cc9b24b4bea
Evgeniy Podgaetskiy <evgeniypodgaetskiy@gmail.com>
2017-12-10 12:21:46 +0200

    base

--- src/components/Events/EventsList.jsx
+++ src/components/Events/EventsList.jsx
@@ -1,17 +1,51 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { getEvents } from '../../actions/events';
 import EventItem from './EventItem';
 import Grid from 'material-ui/Grid';
 import { CircularProgress } from 'material-ui/Progress';
+import moment from 'moment';
+import qs from 'qs';
+
+const timeNow = encodeURIComponent(
+  moment()
+    .utc()
+    .format()
+);
 
 class EventsList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      events: [],
+      isFetching: false
+    };
+  }
+
   componentDidMount() {
-    this.props.getEvents();
+    const params = {
+      type: 'events',
+      direction: 'asc',
+      query: '',
+      sorts: JSON.stringify({
+        startAt: 'asc'
+      }),
+      filters: JSON.stringify({
+        trainingTypes: { id: { neq: 0 } },
+        location: { city: { id: { in: ['1', 18] } } },
+        startAt: { gte: timeNow }
+      }),
+      page: 1,
+      limit: 9
+    };
+
+    fetch(
+      `https://api.gdesport.info/api/search/result${qs.stringify(params, {
+        addQueryPrefix: true
+      })}`
+    );
   }
   render() {
-    const { isFetching, events } = this.props;
+    const { isFetching, events } = this.state;
     return !isFetching ? (
       <Grid container spacing={24}>
         {events.length > 0
@@ -24,16 +58,4 @@ class EventsList extends React.Component {
   }
 }
 
-const mapStateToProps = state => ({
-  events: state.events.items,
-  isFetching: state.events.isFetching
-});
-
-const mapDispatchToProps = dispatch =>
-  bindActionCreators(
-    {
-      getEvents
-    },
-    dispatch
-  );
-export default connect(mapStateToProps, mapDispatchToProps)(EventsList);
+export default EventsList;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import configureStore from './store/configureStore';
 import 'normalize.css';
-//material theme
-import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
-import blue from 'material-ui/colors/blue';
 //material ui roboto font
 import 'typeface-roboto';
-const store = configureStore();
 
-const theme = createMuiTheme({
-  palette: {
-    primary: blue
-  }
-});
-
-ReactDOM.render(
-  <Provider store={store}>
-    <MuiThemeProvider theme={theme}>
-      <App />
-    </MuiThemeProvider>
-  </Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
